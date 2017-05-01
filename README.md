d0ec766255b80a6addc8d3a913ea2f0741744d19
resessh <xuesu-li@retty.me>
2017-05-02 01:02:19 +0900

    :runner: initial commit

---
+++ README.md
@@ -0,0 +1,16 @@
+# cyclejs-quick-start
+
+Cyclejs is Good; Run cyclejs app in seconds
+
+## Install
+`npm i`
+`npm i -g gulp-cli`
+
+## Run
+`gulp`
+
+## Screenshot
+![image](https://cloud.githubusercontent.com/assets/443965/20223177/a81aa7de-a87b-11e6-98d2-5c638b17da54.png)
+
+## License
+MIT
---
+++ src/scripts/main.ts
@@ -0,0 +1,67 @@
+import 'rxjs/Rx';
+import { Observable } from 'rxjs';
+import { VNode } from '@cycle/dom';
+import { DOMSource } from '@cycle/dom/rxjs-typings';
+import { div, input, p, h2, button, span, i, ul, li, makeDOMDriver } from '@cycle/dom';
+import { run } from '@cycle/rxjs-run';
+
+type So = {
+    DOM: DOMSource;
+    props?: {};
+}
+
+type Si = {
+    DOM: Observable<VNode>
+}
+
+function main({DOM}: So): Si {
+    const eventClickAddItemButton$ = DOM.select('#new-todo button').events('click');
+    const eventNewTodoItem$ = DOM.select('#new-todo input').events('change')
+        .map(ev => (ev.target as HTMLInputElement).value)
+        .startWith("hoge");
+
+    return {
+        DOM: eventNewTodoItem$.map((item) => {
+            return div('.container', [
+                div('.row', [
+                    div('.col-xs-8.col-xs-offset-2', [
+                        div('.panel.panel-default', [
+                            div('.panel-heading', [
+                                h2('.panel-title', ['cyclejs todo'])
+                            ]),
+                            div('#new-todo.panel-body.row', [
+                                div('.input-group.col-xs-8.col-xs-offset-2.input-group-lg', [
+                                    input('.form-control'),
+                                    span('.input-group-btn', [
+                                        button('.btn.btn-success', [
+                                            i('.fa.fa-plus')
+                                        ])
+                                    ])
+                                ])
+                            ]),
+                            div('#todo-list.panel-body.row', [
+                                ul('.list-group.col-xs-10.col-xs-offset-1', [
+                                    li('.list-group-item.row', [
+                                        input('.col-xs-1.col-xs-offset-1', {
+                                            attrs: {
+                                                type: 'checkbox'
+                                            }
+                                        }),
+                                        p('.col-xs-8.list-group-item-heading', [item])
+                                    ])
+                                ]),
+                                button('.btn.btn-success.col-xs-4.col-xs-offset-4.btn-lg', [
+                                    i('.fa.fa-check')
+                                ])
+                            ])
+                        ])
+                    ])
+                ])
+            ])
+        })
+    }
+}
+
+run(main, {
+    DOM: makeDOMDriver('#app-container')
+});
