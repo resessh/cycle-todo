5a9edc0183bfddf2ffc2240fa5ff3263bd251264
Ryota Mannari <info@manse.jp>
2017-05-07 16:55:17 +0900

    🔨リファクタ

--- src/scripts/main.ts
+++ src/scripts/main.ts
@@ -2,9 +2,9 @@ import 'rxjs/Rx';
 import { Observable } from 'rxjs';
 import { VNode } from '@cycle/dom';
 import { DOMSource } from '@cycle/dom/rxjs-typings';
-import { CycleDOMEvent, div, input, p, h2, button, span, i, ul, li, makeDOMDriver } from '@cycle/dom';
+import { CycleDOMEvent, form, div, input, p, h2, button, span, i, ul, li, makeDOMDriver } from '@cycle/dom';
 import { run } from '@cycle/rxjs-run';
-import { append, remove, over, lensProp } from 'ramda';
+import { append, remove, over, lensProp, assoc, compose } from 'ramda';
 
 type So = {
     DOM: DOMSource;
@@ -14,6 +14,8 @@ type So = {
 type Si = {
     DOM: Observable<VNode>;
 }
+
+type StateAction = (acc: TodoState) => TodoState;
 
 type TodoItem = {
     title: string;
@@ -26,7 +28,7 @@ type TodoState = {
 }
 const defaultTodoState = { inputValue: '', list: [] };
 
-function renderDOM({inputValue, list}: TodoState): VNode {
+function renderDOM({ inputValue, list }: TodoState): VNode {
     return div('.container', [
         div('.row', [
             div('.col-xs-8.col-xs-offset-2', [
@@ -34,17 +36,21 @@ function renderDOM({inputValue, list}: TodoState): VNode {
                     div('.panel-heading', [
                         h2('.panel-title', ['cyclejs todo'])
                     ]),
-                    div('#new-todo.panel-body.row', [
+                    form('.event-add.panel-body.row', [
                         div('.input-group.col-xs-8.col-xs-offset-2.input-group-lg', [
-                            input('.todo-title.form-control', {
-                                attrs: {
+                            input('.event-input.todo-title.form-control', {
+                                props: {
                                     value: inputValue
                                 }
                             }),
                             span('.input-group-btn', [
-                                button('.add-todo.btn.btn-success', [
-                                    i('.fa.fa-plus')
-                                ])
+                                input('.add-todo.btn.btn-success', {
+                                    attrs: {
+                                        type: 'submit'
+                                    }
+                                }, [
+                                        i('.fa.fa-plus')
+                                    ])
                             ])
                         ])
                     ]),
@@ -52,7 +58,7 @@ function renderDOM({inputValue, list}: TodoState): VNode {
                         ul('.list-group.col-xs-10.col-xs-offset-1', list.map((todoItem, index) =>
                             li('.todo-item.list-group-item.row', [
                                 p('.col-xs-8.list-group-item-heading', [todoItem.title]),
-                                button('.col-xs-2.col-xs-offset-2.btn.btn-success.fa.fa-check', {
+                                button('.event-remove.col-xs-2.col-xs-offset-2.btn.btn-success.fa.fa-check', {
                                     attrs: {
                                         'data-id': index
                                     }
@@ -66,37 +72,38 @@ function renderDOM({inputValue, list}: TodoState): VNode {
     ])
 }
 
-function main({DOM}: So): Si {
-    // DOM event
-    const eventClickAddTodoItem$ = DOM.select('#new-todo button.add-todo').events('click');
-    const eventEnterPressTodoInput$ = DOM.select('#new-todo input.todo-title').events('keypress')
-        .filter(ev => (ev as KeyboardEvent).keyCode == 13);
-    const eventClickRemoveTodoItem$ = DOM.select('#todo-list ul li.todo-item button').events('click');
+function main({ DOM }: So): Si {
+    /**
+     * Event
+     */
+    const eventAdd$ = DOM.select('.event-add').events('submit').do((e) => e.preventDefault()).share();
+    const eventInput$ = DOM.select('.event-input').events('input');
+    const eventRemove$ = DOM.select('.event-remove').events('click');
+    const inputText$ = eventInput$.map(ev => (ev.target as HTMLInputElement).value);
+    const removeIndex$ = eventRemove$.map((ev: CycleDOMEvent) => Number((ev.ownerTarget as HTMLButtonElement).dataset['id']))
 
-    const eventAddItem$ = Observable.merge(eventClickAddTodoItem$, eventEnterPressTodoInput$);
-    const todoTitleValue$ = eventEnterPressTodoInput$.map(ev => (ev.target as HTMLInputElement).value);
-    const todoItemIndexToRemove$ = eventClickRemoveTodoItem$
-        .map((ev: CycleDOMEvent) => Number((ev.ownerTarget as HTMLButtonElement).dataset['id']));
-
-    const todoStateInputValue$: Observable<string> = todoTitleValue$.combineLatest(
-        eventAddItem$, (oldValue, _) => '');
-    const todoStateList$: Observable<TodoItem[]> = Observable.merge(
-        eventAddItem$.withLatestFrom(
-            todoTitleValue$,
-            (_, title) => append({ title, completed: false })
+    /**
+     * State
+     */
+    const defaultState: TodoState = {
+        inputValue: '',
+        list: []
+    };
+    const todoState$ = Observable.merge(
+        eventAdd$.withLatestFrom(
+            inputText$,
+            (_, title) => compose(
+                over(lensProp('list'), append({ title, completed: false })),
+                assoc('inputValue', '')
+            )
         ),
-        todoItemIndexToRemove$.map(
-            index => remove(index, 1)
-        ))
-        .scan((list: TodoItem[], reducer) => {
-            return reducer(list);
-        }, []).startWith([]);
+        inputText$.map(text => assoc('inputValue', text)),
+        removeIndex$.map(index => over(lensProp('list'), remove(index, 1)))
+    ).scan((acc: TodoState, fn: StateAction) => fn(acc), defaultState).startWith(defaultState).do(console.log);
 
-    const todoState$: Observable<TodoState> = Observable.combineLatest(
-        todoStateInputValue$, todoStateList$, (inputValue, list) => {
-            return { inputValue, list }
-        }).startWith(defaultTodoState);
-
+    /**
+     * Sink
+     */
     return {
         DOM: todoState$.map((todoState) => renderDOM(todoState))
     }
