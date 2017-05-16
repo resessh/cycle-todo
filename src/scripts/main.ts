a6f37adf8848a8509bd743f0bbc9f4ab1f80d976
resessh <xuesu-li@retty.me>
2017-05-16 21:12:03 +0900

    :recycle: htmlをいい感じにリファクタ #3
    - cssセレクタをいい感じに
    - form, submitを使っていい感じに

--- src/scripts/main.ts
+++ src/scripts/main.ts
@@ -2,7 +2,7 @@ import 'rxjs/Rx';
 import { Observable } from 'rxjs';
 import { VNode } from '@cycle/dom';
 import { DOMSource } from '@cycle/dom/rxjs-typings';
-import { CycleDOMEvent, div, input, p, h2, button, span, i, ul, li, makeDOMDriver } from '@cycle/dom';
+import { CycleDOMEvent, form, div, input, p, h2, button, span, i, ul, li, makeDOMDriver } from '@cycle/dom';
 import { run } from '@cycle/rxjs-run';
 import { append, remove, over, lensProp } from 'ramda';
 
@@ -34,15 +34,19 @@ function renderDOM({inputValue, list}: TodoState): VNode {
                     div('.panel-heading', [
                         h2('.panel-title', ['cyclejs todo'])
                     ]),
-                    div('#new-todo.panel-body.row', [
+                    form('.event-add.panel-body.row', [
                         div('.input-group.col-xs-8.col-xs-offset-2.input-group-lg', [
-                            input('.todo-title.form-control', {
-                                attrs: {
+                            input('.event-input.form-control', {
+                                props: {
                                     value: inputValue
                                 }
                             }),
                             span('.input-group-btn', [
-                                button('.add-todo.btn.btn-success', [
+                                input('.add-todo.btn.btn-success', {
+                                    attrs: {
+                                        type: 'submit'
+                                    }
+                                }, [
                                     i('.fa.fa-plus')
                                 ])
                             ])
@@ -52,7 +56,7 @@ function renderDOM({inputValue, list}: TodoState): VNode {
                         ul('.list-group.col-xs-10.col-xs-offset-1', list.map((todoItem, index) =>
                             li('.todo-item.list-group-item.row', [
                                 p('.col-xs-8.list-group-item-heading', [todoItem.title]),
-                                button('.col-xs-2.col-xs-offset-2.btn.btn-success.fa.fa-check', {
+                                button('.event-remove.col-xs-2.col-xs-offset-2.btn.btn-success.fa.fa-check', {
                                     attrs: {
                                         'data-id': index
                                     }
@@ -68,20 +72,17 @@ function renderDOM({inputValue, list}: TodoState): VNode {
 
 function main({DOM}: So): Si {
     // DOM event
-    const eventClickAddTodoItem$ = DOM.select('#new-todo button.add-todo').events('click');
-    const eventEnterPressTodoInput$ = DOM.select('#new-todo input.todo-title').events('keypress')
-        .filter(ev => (ev as KeyboardEvent).keyCode == 13);
-    const eventClickRemoveTodoItem$ = DOM.select('#todo-list ul li.todo-item button').events('click');
+    const eventAdd$ = DOM.select('.event-add').events('submit').do(ev => ev.preventDefault()).share();
+    const eventInput$ = DOM.select('.event-input').events('input');
+    const eventRemove$ = DOM.select('.event-remove').events('click');
 
-    const eventAddItem$ = Observable.merge(eventClickAddTodoItem$, eventEnterPressTodoInput$);
-    const todoTitleValue$ = eventEnterPressTodoInput$.map(ev => (ev.target as HTMLInputElement).value);
-    const todoItemIndexToRemove$ = eventClickRemoveTodoItem$
-        .map((ev: CycleDOMEvent) => Number((ev.ownerTarget as HTMLButtonElement).dataset['id']));
+    const todoTitleValue$ = eventInput$.map(ev => (ev.target as HTMLInputElement).value);
+    const todoItemIndexToRemove$ = eventRemove$.map((ev: CycleDOMEvent) => Number((ev.ownerTarget as HTMLButtonElement).dataset['id']));
 
     const todoStateInputValue$: Observable<string> = todoTitleValue$.combineLatest(
-        eventAddItem$, (oldValue, _) => '');
+        eventAdd$, (oldValue, _) => '');
     const todoStateList$: Observable<TodoItem[]> = Observable.merge(
-        eventAddItem$.withLatestFrom(
+        eventAdd$.withLatestFrom(
             todoTitleValue$,
             (_, title) => append({ title, completed: false })
         ),
